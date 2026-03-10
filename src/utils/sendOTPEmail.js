aac4a389c2311d5e40709d0292538c8b7cf0a503
moataz-saad <161377495+moataz-saad@users.noreply.github.com>
2026-03-10 20:10:59 +0200

    Refactor sendOTPEmail to improve readability

--- src/utils/sendOTPEmail.js
+++ src/utils/sendOTPEmail.js
@@ -1,14 +1,14 @@
 const nodemailer = require("nodemailer");
-
 const sendOTPEmail = async (email, otp) => {
   try {
+    const emailPass = process.env.EMAIL_PASS ? process.env.EMAIL_PASS.replace(/\s/g, "") : "";
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465, // منفذ Gmail الآمن
+      port: 465, 
       secure: true, 
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS.replace(/\s/g, ""), // بيمسح أي مسافات أوتوماتيك لو نسيت
+        pass: emailPass, 
       },
     });
 
@@ -17,14 +17,49 @@ const sendOTPEmail = async (email, otp) => {
       to: email,
       subject: "Verification Code",
       text: `Your OTP is: ${otp}`,
+      html: `<b>Your verification code is: ${otp}</b>`, // يفضل إضافة HTML برضه
     };
 
     const info = await transporter.sendMail(mailOptions);
     console.log("✅ Email Sent: " + info.response);
-  } catch (error) {
+  } 
+  catch (error) {
     console.error("❌ Nodemailer Details:", error.message);
-    throw error; // بنرمي الخطأ عشان الـ Controller يحس بيه
+    throw error; 
   }
 };
 
 module.exports = sendOTPEmail;
+
+
+
+
+
+// const sendOTPEmail = async (email, otp) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: "smtp.gmail.com",
+//       port: 465, // منفذ Gmail الآمن
+//       secure: true, 
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS.replace(/\s/g, ""), // بيمسح أي مسافات أوتوماتيك لو نسيت
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: `"Beach Flow" <${process.env.EMAIL_USER}>`,
+//       to: email,
+//       subject: "Verification Code",
+//       text: `Your OTP is: ${otp}`,
+//     };
+
+//     const info = await transporter.sendMail(mailOptions);
+//     console.log("✅ Email Sent: " + info.response);
+//   } catch (error) {
+//     console.error("❌ Nodemailer Details:", error.message);
+//     throw error; // بنرمي الخطأ عشان الـ Controller يحس بيه
+//   }
+// };
+
+// module.exports = sendOTPEmail;
