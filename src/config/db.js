e4b645cef3eed42ec0b96a747971b57e7f6dbd39
moataz-saad <161377495+moataz-saad@users.noreply.github.com>
2026-03-10 17:05:05 +0200

    Refactor database connection to use DATABASE_URL
    Updated Sequelize configuration to use DATABASE_URL and added SSL options for secure connection.

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,17 @@
 const { Sequelize } = require("sequelize");
 require('dotenv').config({ path: require('path').join(__dirname, '../../.env') });
 
-
-
-
-
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,     
-  process.env.DB_USER,     
-  process.env.DB_PASSWORD, 
-  {
-    host: process.env.DB_HOST, 
-    port: process.env.DB_PORT, 
-    dialect: "postgres",
-    logging: false, 
+// Railway بتوفر متغير اسمه DATABASE_URL فيه كل حاجة
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // السطر ده هو اللي هيحل مشكلة الـ Crash في الربط
+    }
   }
-);
+});
 
 const connectDB = async () => {
   try {
